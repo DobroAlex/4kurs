d32a225a1d57ac423fe10b03d363ab3577d1267f
Alex <alexkoshernosiegov@gmail.com>
2018-09-30 11:40:55 +0300

    Структуры для априори

---
+++ IAD/Apriori/Apriori/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Apriori.DataStructures;
+namespace Apriori
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
