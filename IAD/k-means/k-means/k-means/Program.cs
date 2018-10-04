88990e8928b7c341950e3f2e571d338b9e5a56a0
Alex <alexkoshernosiegov@gmail.com>
2018-10-04 16:09:38 +0300

    Пишется к-средний

---
+++ IAD/k-means/k-means/k-means/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using KMeans.DataStructures;
+namespace KMeans
+{
+    class MainClass
+    {
+
+
+        public static void Main(string[] args)
+        {
+            PointCollection AllPoints = new PointCollection();
+            AllPoints.Add(new KMeans.DataStructures.Point(0, 1, 1.5));
+            AllPoints.Add(new KMeans.DataStructures.Point(1, 5, 4));
+            AllPoints.Add(new KMeans.DataStructures.Point(2, 2, 9));
+            AllPoints.Add(new KMeans.DataStructures.Point(3, 4, 3));
+            AllPoints.Add(new KMeans.DataStructures.Point(4, 4, 4));
+            AllPoints.Add(new KMeans.DataStructures.Point(5, 6, 1.8));
+            AllPoints.Add(new KMeans.DataStructures.Point(6, 7, 13));
+        }
+    }
+}
