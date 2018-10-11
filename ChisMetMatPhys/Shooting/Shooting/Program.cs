bb30ab3bb24f24260039d9226fa091a35bd46d59
Alex <alexkoshernosiegov@gmail.com>
2018-10-11 15:22:15 +0300

    СТрельба

---
+++ ChisMetMatPhys/Shooting/Shooting/Program.cs
@@ -0,0 +1,14 @@
+﻿/*https://www.codewithc.com/c-program-for-shooting-method/*/
+using System;
+
+namespace Shooting
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double x0, y0, xn, yn;
+
+        }
+    }
+}
