7988f1efdb510d98ca5461c5f41a972b9f6133fe
Alex <alexkoshernosiegov@gmail.com>
2018-10-22 21:30:24 +0300

    Сценарии  тестового запуска

---
+++ NetWorks/Lab3/TestLinear.sh
@@ -0,0 +1,4 @@
+#https://stackoverflow.com/questions/556405/what-do-real-user-and-sys-mean-in-the-output-of-time1
+
+#Запускаем клиенты последовательно
+time ./ClientsTest.sh
---
+++ NetWorks/Lab3/TestParallel.sh
@@ -0,0 +1,5 @@
+#https://stackoverflow.com/questions/556405/what-do-real-user-and-sys-mean-in-the-output-of-time1
+
+#Запускаем клиенты параллельно
+
+time ./ClientsParallelTest.sh
