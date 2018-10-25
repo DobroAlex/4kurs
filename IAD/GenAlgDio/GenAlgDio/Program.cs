8e637d3e41b0dee4e29d0926f76e026e81aed0d2
Alex <alexkoshernosiegov@gmail.com>
2018-10-25 22:53:36 +0300

    Структуры данных

---
+++ IAD/GenAlgDio/GenAlgDio/Program.cs
@@ -0,0 +1,19 @@
+﻿/*  http://algolist.manual.ru/ai/ga/dioph.php   */
+/*  http://algolist.manual.ru/ai/ga/breakdown.php   */
+using System;
+using System.Collections.Generic;
+namespace GenAlgDio
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            
+
+        }
+        public static int Equation(int a, int b, int c, int d)
+        {
+            return a + 2 * b + 3 * c + 4 * d;
+        }
+    }
+}
