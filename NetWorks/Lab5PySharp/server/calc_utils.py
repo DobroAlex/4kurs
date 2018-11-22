1f6167c476b546c8866987ff1a8856976c16ad6d
Alex <alexkoshernosiegov@gmail.com>
2018-11-22 19:16:49 +0300

    Пишется

--- NetWorks/Lab5PySharp/server/calc_utils.py
+++
@@ -1,6 +0,0 @@
-import math
-def is_prime(num: int) -> bool:
-    for i in range (2, int(math.sqrt(num))):
-        if num % i != 0 :
-            return False
-    return True
--- NetWorks/Lab5PySharp/server/server.py
+++ NetWorks/Lab5PySharp/server/server.py
@@ -1,38 +1,26 @@
 import socket
-import multiprocessing
+import asyncio
 import sys
-import calc_utils as CU
+import client
+import utils
 GLOBAL_NUM_OF_CLIENTS = 0
 BUFF_SIZE = 1048576     #1 Mebibyte, 2^20
-def work_routine(client_socket: socket.socket, address):
-    global GLOBAL_NUM_OF_CLIENTS
-    CLIENT_ID = str(GLOBAL_NUM_OF_CLIENTS) + str(client_socket.getsockname())
-    GLOBAL_NUM_OF_CLIENTS += 1
-    print("{1} : Connected to {0}".format(CLIENT_ID, client_socket.getsockname()))
-    while True:
-        data = client_socket.recv(BUFF_SIZE)
-        data = data.decode("utf8")
-        print("Recived {0}".format(data))
-        if not data:
-            print("{0}  has disconnected".format(client_socket.getsockname()))
-            client_socket.close()
-            break
-        client_socket.sendall(("We will now search for {0}  in news".format(data)).encode("utf8"))
-    client_socket.close()
+
         
 def main():
-    SERVER_ADDRESS = ('localhost', 6868)
+    global GLOBAL_NUM_OF_CLIENTS
+    SERVER_ADDRESS = ('localhost', utils.find_free_socket('localhost', 6868))
     serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serv.bind(SERVER_ADDRESS)
     serv.listen(100)
     print("Server have started at {0}".format(SERVER_ADDRESS))
+    tasks = list()
     while True :
-        print("Waiting for clients")
-        client_sock, addr = serv.accept()
-        serv.setblocking(0)        
-        P = multiprocessing.Process(target=work_routine(client_sock, addr))
-        P.start()
-        P.join()
+        ioloop = asyncio.get_event_loop()
+        tasks.append(ioloop.create_task(client.client(serv, GLOBAL_NUM_OF_CLIENTS).routine() ))
+        GLOBAL_NUM_OF_CLIENTS += 1
+        wait_tasks = asyncio.wait(tasks)
+        ioloop.run_until_complete(wait_tasks)
         #client_sock, addr = serv.accept()
         #print("Connected to {0}".format(addr))
         #while True:
@@ -50,7 +38,7 @@ def main():
         #        break
             #print("Data recived from {0}:{1}".format(addr, data.decode("utf8")))
     serv.close()
-    
+    ioloop.close() 
 
 
 
