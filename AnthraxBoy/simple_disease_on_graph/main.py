f6f1e847adc72e58111d67205d59b567295a68d7
Alex <alexkoshernosiegov@gmail.com>
2018-12-02 16:12:43 +0300

    Major infection logic update
    There is now agent -- a person which will move across the locations and will try to infect all population in given place

--- AnthraxBoy/simple_disease_on_graph/main.py
+++ AnthraxBoy/simple_disease_on_graph/main.py
@@ -2,14 +2,17 @@ import networkx as nx   #networkx >=2.3 required
 import matplotlib as mpl    #matplotlib as fresh as possible
 from matplotlib import pyplot
 from  matplotlib.pyplot import show, draw
+import math
 import sys  #for future parsing of sys.args
 import os   #for file handling  #TODO: should be removed from here ans used only in places.py
 import place    #class for modeling places which may or may not be infected
 import possible_states_enum as PSE  #all possibles condition of places  
 import graph_utils as GU    #some basic utils for supporting  drawing & iterating over graph
 import random   #for random int and float numbers generation
-import json
-def do_visit(G: nx.Graph, is_node_visited_only_once: bool = False, start_node: int = -1): 
+import json     #for JSON parse/dump
+import infection as Infection
+import person as Person
+def do_visit(G: nx.Graph, agent:Person.person, is_node_visited_only_once: bool = False, start_node: int = -1) -> None: 
     if start_node == -1:
         current_node  = random.randint(0, G.__len__() - 1)
     else :
@@ -27,20 +30,21 @@ def do_visit(G: nx.Graph, is_node_visited_only_once: bool = False, start_node: i
                     break
                 else:
                     node_to_visit = random.randint(0, G.__len__() - 1)
-        probability = random.uniform(0 , 1)     #TODO : change to normal probability calculation 
-        if probability  > 0.5:
-            G.nodes[node_to_visit]['data'].state = PSE.possible_state.infected
-        else :
-            G.nodes[node_to_visit]['data'].state = PSE.possible_state.not_infected
+        for target_person in  G.nodes[node_to_visit]['data'].persons:
+            for disease_of_agent in agent.infected_with:
+                probability = 1.0 - math.exp(G.nodes[node_to_visit]['data'].direction * math.log(1-target_person.receptivity*agent.infected_with[disease_of_agent]))          
+                if probability >= 0.5:
+                    target_person.infected_with[disease_of_agent] = agent.infected_with[disease_of_agent]
+                    G.nodes[node_to_visit]['data'].state == PSE.possible_state.infected
+                else:
+                    if G.nodes[node_to_visit]['data'].state !=  PSE.possible_state.infected :
+                        G.nodes[node_to_visit]['data'].state = PSE.possible_state.not_infected
+        
         current_node = node_to_visit
 
 def main():
+    agent = Person.person(age=35, sex = 'm', receptivity=0.5, infected_with = {'Ветрянка': 0.1, 'ОРВИ': 0.5, 'СПИДОРАК': 0.8}  )
     G = nx.Graph()  #creates new empty graph
-
-    #TODO: change to JSON
-    #parsed_places = place.place.parse_list_from_txt_file("resources/places.txt")    #parsing names from file    
-    #with open("resources/places.json") as data_file:
-    #    parsed_places = json.load(data_file)
     parsed_places = place.place.parse_list_of_places_from_json(path_to_file="resources/places.json")
     for item in parsed_places:
         G.add_node(parsed_places.index(item), data=item, pos = (item.latitude, item.longitude ))    #adding places as nodes to graph
@@ -49,7 +53,9 @@ def main():
     while not (nx.is_connected(G)): #while each node doesn't have at least one edge
         G.add_edge(random.randint(0, G.__len__()-1), random.randint(0, G.__len__()-1))  #adding random edge
     GU.graph_show_and_save(G, "graph", to_save=False)
-    do_visit(G, start_node = 0) #see do_visit()
+    do_visit(G, agent = agent,  start_node = 0) #see do_visit()
     GU.graph_show_and_save(G, "infected_graph", to_save = False)
+
+
 if __name__ == "__main__":
     main()
