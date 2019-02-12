30a6709be90fecbdc8e9546fa2671913216ead1b
Alex <alexkoshernosiegov@gmail.com>
2019-02-12 22:17:09 +0300

    Added class for possible person's  states: not-infected/recovered, infected, dead/buried

--- AnthraxBoy/simple_disease_on_graph/possible_person_states_enum.py
+++ AnthraxBoy/simple_disease_on_graph/possible_person_states_enum.py
@@ -2,6 +2,8 @@ import enum
 
 
 class PossiblePersonState(enum):
-    not_infected = 0  # save and unexposed, non-suspicious
-    dead = 1  # dead but  not buried, may still be source of infection
-    buried = 2  # dead and six feet under, can't be source of infection no more
+    not_infected = 0  # save and unexposed, non-suspicious, synonymous for alive. Person may switch to that state
+    # after recovering from all infections
+    infected = 1    # infected with any infection. Should be set if person infected with any infection
+    dead = 2  # dead but  not buried, may still be source of infection
+    buried = 3  # dead and six feet under, can't be source of infection no more
