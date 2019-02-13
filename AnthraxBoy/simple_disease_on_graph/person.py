acc21d99ae2d867f9e68bfb83ddb3d73595b49de
Alex <alexkoshernosiegov@gmail.com>
2019-02-13 18:37:17 +0300

    Reworking person parsing

--- AnthraxBoy/simple_disease_on_graph/person.py
+++ AnthraxBoy/simple_disease_on_graph/person.py
@@ -1,14 +1,17 @@
 import random
+import scipy
+from possible_person_states_enum import PossiblePersonState as PossiblePersonState
 class person:
-    def __init__(self, age: int, sex: chr, receptivity: float, infected_with: dict = dict()):
+    def __init__(self, age: int, sex: chr, receptivity: float, infected_with: dict = dict, person_state: PossiblePersonState = PossiblePersonState.not_infected):
         self.age = age
         self.sex = sex
         self.receptivity = receptivity
         self.infected_with = infected_with
+        self.person_state = person_state
 
     def __str__(self):
         return "Age : " + str(self.age) + " sex : " + self.sex + " receptivity : " + str(
-            self.receptivity) + " infected with : " + str(self.infected_with)
+            self.receptivity) + " infected with : " + str(self.infected_with) + "current state : " + str
 
     @staticmethod
     def get_random_age(start: int = 18, end: int = 95) -> int:
@@ -16,15 +19,13 @@ class person:
 
     @staticmethod
     def parse_person_from_json(json_object: dict):
-        retPerson = person(age=json_object.get("age", None),
-                           sex=json_object.get("sex", "None"),
-                           receptivity=json_object.get("receptivity", 0.1)
+        ret_person = person(age=json_object.get("age", None),
+                           sex=json_object.get("sex", None),
+                           receptivity=json_object.get("receptivity", random.random())
                            )
-        if retPerson.receptivity is None:
-            retPerson.receptivity == random.uniform(0.1, 1)
-        if retPerson.infected_with is None:
-            retPerson.infected_with = dict()
-        return retPerson
+        if ret_person.infected_with is None:
+            ret_person.infected_with = dict()
+        return ret_person
 
     @staticmethod
     def parse_persons_from_place_json(json_object: list) -> list:
