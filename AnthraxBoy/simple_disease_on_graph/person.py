2550c065cd72a4b50fceec35d0c3d06bd1b5c9ba
Alex <alexkoshernosiegov@gmail.com>
2019-04-01 21:43:39 +0300

    M8 CummIt

--- AnthraxBoy/simple_disease_on_graph/person.py
+++ AnthraxBoy/simple_disease_on_graph/person.py
@@ -1,4 +1,6 @@
 import random
+from typing import List
+
 from scipy import stats
 from possible_person_states_enum import PossiblePersonState as PossiblePersonState
 
@@ -13,8 +15,8 @@ class person:
         self.person_state = person_state
 
     def __str__(self):
-        return "Age : " + str(self.age) + " sex : " + self.sex + " receptivity : " + str(
-            self.receptivity) + " infected with : " + str(self.infected_with) + "current state : " + str(self.person_state)
+        return f"Age : {self.age} sex : {self.sex} receptivity : {self.receptivity} infected with : " \
+                   f"{self.infected_with} current state : {self.person_state}"
 
     @staticmethod
     def get_random_age(start: int = 18, end: int = 95) -> int:
@@ -22,19 +24,21 @@ class person:
 
     @staticmethod
     def parse_person_from_json(json_object: dict):
-        ret_person = person(age=json_object.get("age", None),
-                            sex=json_object.get("sex", None),
-                            receptivity=json_object.get("receptivity", 0.111 if stats.norm.rvs(loc=0.5,
-                                                                                             scale=0.25 ** 0.5) <= 0 else 0.99 if stats.norm.rvs(
-                                loc=0.5, scale=0.25 ** 0.5) >= 1 else stats.norm.rvs(loc=0.5, scale=0.25 ** 0.5))
-
-                            )
+        ret_person = person(
+            age=json_object.get("age", None),
+            sex=json_object.get("sex", None),
+            receptivity=json_object.get("receptivity", 0.111 if stats.norm.rvs(
+                loc=0.5,
+                scale=0.25 ** 0.5) <= 0 else 0.99 if stats.norm.rvs(loc=0.5, scale=0.25 ** 0.5) >= 1
+                    else stats.norm.rvs(loc=0.5, scale=0.25 ** 0.5)
+            )
+        )
         if ret_person.infected_with is None:
-            ret_person.infected_with = dict()
+            ret_person.infected_with = {}
         return ret_person
 
     @staticmethod
-    def parse_persons_from_place_json(json_object: list) -> list:
-        retVal = list()
+    def parse_persons_from_place_json(json_object: List) -> List:
+        ret_val = list()
         for json_str in json_object:
-            retVal.append(person.parse_person_from_json(json_str))
+            ret_val.append(person.parse_person_from_json(json_str))
